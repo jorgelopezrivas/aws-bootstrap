2ad144592c511250ed8a1b9ec289cfe898249f2c
Jorge L. Rivas <triebjlr@gmail.com>
2021-04-28 15:40:32 -0500

    add codebuild and codedeploy specs

--- cloudformation-stack/deploy-infra.sh
+++ cloudformation-stack/deploy-infra.sh
@@ -4,9 +4,23 @@ STACK_NAME=awsbootstrap
 REGION=us-east-1
 CLI_PROFILE=awsbootstrap
 EC2_INSTANCE_TYPE=t2.micro
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
+  --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
+
+echo -e "\n\n=========== Deploying setup.yaml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # Deploy ClouydFormation template
-echo -e "\n\n========= Deploying main.yml ==========="
+echo -e "\n\n========= Deploying main.yaml ==========="
 aws cloudformation deploy \
  --region $REGION \
  --profile $CLI_PROFILE \
@@ -16,4 +30,11 @@ aws cloudformation deploy \
  --capabilities CAPABILITY_NAMED_IAM \
  --parameter-overrides \
  EC2InstanceType=$EC2_INSTANCE_TYPE
+
+# If the deploy succeeded, show the DNS name of the created instance
+if [ $? -eq 0 ]; then
+  aws cloudformation list-exports \
+    --profile awsbootstrap \
+    --query "Exports[?Name=='InstanceEndpoint'].Value" 
+fi
  
